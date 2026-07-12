9c8d28c912d31d6609508b800047ec5917409d4b
benimafoua-cmd <benimafoua@gmail.com>
2026-07-12 17:08:49 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,15 +1,1 @@
-<?php
-// db.php
-$host = 'localhost';
-$dbname = 'franprix_brazzaville';
-$username = 'root'; 
-$password = '';     
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>

2e5734c3e9bbae7b8dc6db4ab64e52b7c3932304
benimafoua-cmd <benimafoua@gmail.com>
2026-07-13 01:28:42 +0100

    Change database password and update comments
    Updated database connection password and instructions.

--- db.php
+++ db.php
@@ -1,12 +1,10 @@
 <?php
-// db.php (Version Définitive sans erreur de frappe)
-
 $host = '://clever-cloud.com'; 
 $dbname = 'bt3e3ek2thxfoiwadcjb'; 
 $username = 'u7jckigghgxmlglf'; 
 
-// Remplace uniquement le texte ci-dessous par ton vrai mot de passe en minuscules
-$password = 'METS_TON_PASSWORD_ICI';     
+// COUPE ET COLLE LE CODE DE LA CASE PASSWORD DE CLEVER CLOUD ICI :
+$password = 'uTfq98EATqXndGlndBQG';     
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
